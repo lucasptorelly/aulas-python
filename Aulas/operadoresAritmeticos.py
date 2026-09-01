4e1ebe94c4ea80ecb456f12ed4aadc63f50c49eb
Lucas <lucasptorelly@gmail.com>
2026-09-01 19:57:28 -0300

    Conteudo de operardores aritmeticos

---
+++ Aulas/operadoresAritmeticos.py
@@ -0,0 +1,19 @@
+#OPERADORES MATEMÁTICOS
+NUMERO1 = 10
+NUMERO2 = 20
+
+soma = NUMERO1 + NUMERO2
+
+subtracao = NUMERO1 - NUMERO2
+
+multiplicacao = NUMERO1 * NUMERO2
+
+divisao = NUMERO1 / NUMERO2
+
+resto = NUMERO1 % NUMERO2
+
+print("soma dos número", soma)
+print("subtração dos número", subtracao)
+print("multiplicação dos número", multiplicacao)
+print("divisão dos número", divisao)
+print("resto dos número", resto)
