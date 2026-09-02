3249635ff1c7cc6401a5f0a00cb2636136e8934b
Lucas <lucasptorelly@gmail.com>
2026-09-02 20:44:05 -0300

    Atividade 2

---
+++ Aulas/operadoresLogicos.py
@@ -0,0 +1,23 @@
+    #operates
+"""
+        #atribuição
+        = -> variavel = 10
+        ! = Não, not, contrario
+        SIM -> !Sim = Não
+    
+
+        #Comparação
+        
+        != se for diferente... 
+        
+        >= -> se for maior e igual retorna true, contrario retorna falso
+        <= -> se for menor ou igual retorna true, contrario retorna falso
+        
+        Para mais comparações
+        and
+        idade = 18
+        idade == 18 and 
+        
+"""
+
+#teste
