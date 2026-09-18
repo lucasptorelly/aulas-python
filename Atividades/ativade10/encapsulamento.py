b5e89fb1aa31792dcfd7ce8041c52d5ba0bf8e4d
Lucas <lucasptorelly@gmail.com>
2026-09-18 19:41:34 -0300

    Atividade 5

--- Atividades/ativade10/encapsulamento.py
+++ Atividades/ativade10/encapsulamento.py
@@ -1,4 +1,5 @@
 class Produto:
+
     def __init__(self, nome, preco, quantidade_estoque):
         self.__nome = nome
         self.__preco = preco
@@ -9,3 +10,30 @@ class Produto:
             self.__quantidade_estoque = self.__quantidade_estoque + quantidade
         else:
             print("erro: Quantidade inválida")
+
+    def realizar_venda(self, quantidade):
+        if quantidade > 0 and quantidade <= self.__quantidade_estoque:
+            self.__quantidade_estoque = self.__quantidade_estoque - quantidade
+
+        else:
+            print("Venda negada: Estoque insuficiente")
+    def aplicar_desconto(self, percentual):
+        if percentual > 0 and percentual <= 80:
+            desconto = self.__preco * percentual / 100
+            self.__preco = self.preco - desconto
+        else:
+            print("Erro: Desconto inválido")
+    def exibir_resumo(self):
+        print("Nome:", self.__nome)
+        print("Preço: R$", self.__preco)
+        print("Quantidade em estoque:", self.__quantidade_estoque)
+
+
+meu_produto= Produto("Notebook", 3000, 10)
+meu_produto.__quantidade_estoque = -50
+meu_produto.__preco = -100
+meu_produto.realizar_venda(9999)
+meu_produto.exibir_resumo()
+print(meu_produto.__dict__)
+
+
