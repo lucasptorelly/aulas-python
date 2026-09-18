b42ea447dac2c55aba7768c7dcf2b5fd65c18413
Lucas <lucasptorelly@gmail.com>
2026-09-17 21:47:20 -0300

    Atividade 5

---
+++ Atividades/ativade10/encapsulamento.py
@@ -0,0 +1,11 @@
+class Produto:
+    def __init__(self, nome, preco, quantidade_estoque):
+        self.__nome = nome
+        self.__preco = preco
+        self.__quantidade_estoque = quantidade_estoque
+
+    def adicionar_estoque(self, quantidade):
+        if quantidade > 0:
+            self.__quantidade_estoque = self.__quantidade_estoque + quantidade
+        else:
+            print("erro: Quantidade inválida")
